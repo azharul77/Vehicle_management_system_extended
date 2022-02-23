8b0ea33b7881eb6b4292df915b4e16977360c7e1
azharul77 <azharulamin.cse@gmail.com>
2022-02-23 20:26:50 +0600

    i added print in every line of python code for debug

--- models/download.py
+++ models/download.py
@@ -1,34 +1,38 @@
 from odoo import models, fields, _, api
-from odoo.exceptions import UserError
-import PyPDF2
-import tempfile
-import os
-import base64
-import io
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class download(models.Model):
-    _inherit = ['sale.order', 'stock.picking']
+    _inherit = ['sale.order']
     transaction_ids = fields.Many2many('payment.transaction', 'order_ids', string="Transactions")
     tag_ids = fields.Many2many('crm.tag', 'tag_ids', string='Tags')
 
     def generate_awb_pdf(self):
         sale_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
-        pickings = self.env['stock.picking'].search([('origin', 'in', [o.name for o in sale_orders])])
         attachment_ids = self.env['ir.attachment'].search(
-            [('res_model', '=', 'stock.picking'), ('res_id', 'in', [p.id for p in pickings])])
+            [('res_model', '=', 'sale.order'), ('res_id', 'in', [o.id for o in sale_orders])])
         result = b''
 
         for rec in attachment_ids:
+            print(attachment_ids)
+            print(rec)
             result += rec.datas
+            print(result)
             attachment_obj = self.env['ir.attachment']
+            print(attachment_obj)
             attachment_id = attachment_obj.sudo().create(
                 {'name': "name", 'store_fname': 'awb.pdf', 'datas': rec.datas})
+            print(attachment_id)
             download_url = '/web/content/' + str(attachment_id.id) + '?download=true'
+            print(download_url)
             self.get_report(download_url)
+            print(self.get_report(download_url))
 
     def get_report(self, download_url):
         base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+        print(base_url)
         return {
             'name': 'Report',
             'type': 'ir.actions.act_url',
